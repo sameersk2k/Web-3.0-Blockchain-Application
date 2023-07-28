bc16a58ec6527a06898456d18f1f0f007ea9336f
Sameer Shaik <115322069+sameersk2k@users.noreply.github.com>
2023-07-28 18:21:02 +0530

    Update hardhat.config.js

--- project_web3.0-main/smart_contract/hardhat.config.js
+++ project_web3.0-main/smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/pBEgWLtPknEFcCp366Sh3OIdbI9z7SsW',
+      url: 'https://eth-sepolia.g.alchemy.com/v2/4D6QjKLXUFex1R0JTVGuY7TbL8c3SsSK',
       accounts: ['your metamask private key'],
     },
   },
